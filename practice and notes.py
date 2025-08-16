a05ef1155e3a777eef386612e7bcd93f0e482390
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2025-08-16 06:38:29 +0530

    Create practice and notes.py

---
+++ practice and notes.py
@@ -0,0 +1,2 @@
+practice link https://www.hackerrank.com/domains/python?badge_type=python                                                                                
+notes section https://www.codewithharry.com/notes
